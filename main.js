9fe8ed91fd0d8757412d1d25f178295d304a0258
willihernandez <claudu0420@gmail.com>
2023-04-22 09:43:44 -0500

    se agrega la primera preentrega de curso J.S

---
+++ main.js
@@ -0,0 +1,39 @@
+alert("ingresa numero del producto, para salir ingrese 0");
+let seleccioneProducto = parseInt(prompt("1=buzo $45.000,   2=jean $55.000,  3=tenis $220.000,  4=gorra $ 23.500"));
+let seleccioneCantidad;
+const iva =x => x * 0.21;
+let total= 0;
+
+const cantidad = (cant, precio) => {
+    return cant * precio
+}
+
+while (seleccioneProducto != 0 ) {
+    switch(seleccioneProducto) {
+        case 1: 
+           seleccioneCantidad = parseInt(prompt("el producto de su eleccion es buzo, indique la cantidad"))
+                    total += cantidad(seleccioneCantidad, 45000)
+        break;  
+        case 2: 
+           seleccioneCantidad = parseInt(prompt("el producto de su eleccion es jean, indique la cantidad"))
+                    total += cantidad(seleccioneCantidad, 55000)
+        break;            
+        case 3: 
+           seleccioneCantidad =parseInt(prompt("el producto de su eleccion es tenis, indique la cantidad"))
+                    total += cantidad(seleccioneCantidad, 220000)
+        break;            
+        case 4: 
+            seleccioneCantidad = parseInt(prompt("el producto de su eleccion es gorra, indique la cantidad"))
+                    total += cantidad(seleccioneCantidad, 23500)
+        break;   
+
+        default: prompt("producto no encontrado")
+        break;           
+    }
+    seleccioneProducto = parseInt(prompt("1=buzo $45.000 2=jean $55.000 3=tenis $220.000 4=gorra $ 23.500"));
+}
+
+
+
+alert("el total de su compra es : "  + total);
+
