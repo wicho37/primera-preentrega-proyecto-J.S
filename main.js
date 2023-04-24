2a7f6da17e853d2c4c8f529ca5f5c4f6be32e602
willihernandez <claudu0420@gmail.com>
2023-04-23 21:51:14 -0500

    algoritmo de compra sumando el iva al total

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-alert("ingresa numero del producto, para salir ingrese 0");
+
+
+ alert("ingresa numero del producto, para salir ingrese 0");
 let seleccioneProducto = parseInt(prompt("1=buzo $45.000,   2=jean $55.000,  3=tenis $220.000,  4=gorra $ 23.500"));
 let seleccioneCantidad;
 const iva =x => x * 0.21;
 let total= 0;
+let validacionCiclo = true;
 
 const cantidad = (cant, precio) => {
     return cant * precio
 }
 
-while (seleccioneProducto != 0 ) {
+while(validacionCiclo) {
     switch(seleccioneProducto) {
         case 1: 
            seleccioneCantidad = parseInt(prompt("el producto de su eleccion es buzo, indique la cantidad"))
@@ -26,14 +29,18 @@ while (seleccioneProducto != 0 ) {
             seleccioneCantidad = parseInt(prompt("el producto de su eleccion es gorra, indique la cantidad"))
                     total += cantidad(seleccioneCantidad, 23500)
         break;   
+        case 0:
+            validacionCiclo=false;
 
-        default: prompt("producto no encontrado")
-        break;           
+
+         default: prompt("producto no encontrado")
+         break;           
     }
     seleccioneProducto = parseInt(prompt("1=buzo $45.000 2=jean $55.000 3=tenis $220.000 4=gorra $ 23.500"));
 }
 
 
 
-alert("el total de su compra es : "  + total);
+alert("el total de su compra es : "  + (iva(total) + total));
+
 
